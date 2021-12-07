cc73d69ad4acb270d7e4367f412067d81df8d575
Ishaan Nagpal <ishaan18nagpal@gmail.com>
2021-12-07 15:08:40 +0530

    Initial Commit

--- Coming_Soon/settings.py
+++ Coming_Soon/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 import django_heroku
 import _locale
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4=041((lc*ypi3$!5gwng_p)ui$)3^)p_(-kg*%66gg6d^c6b9'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
