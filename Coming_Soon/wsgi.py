7c2be0cea4ba0dd1fc63caf2e9f8770a42f3c34f
Ishaan Nagpal <ishaan18nagpal@gmail.com>
2021-12-07 14:45:28 +0530

    Initial Commit

---
+++ Coming_Soon/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Coming_Soon project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Coming_Soon.settings')
+
+application = get_wsgi_application()
